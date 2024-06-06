1cbdec8411070c00163f058b19c779b09a6827fb
Rutvik Pandya <rutvikpandya00@gmail.com>
2024-06-06 05:46:58 +0530

    Angular-Meme-Generator

--- src/app/generator/generator.component.ts
+++ src/app/generator/generator.component.ts
@@ -1,7 +1,8 @@
-import { Component, viewChild } from '@angular/core';
+import { Component, ViewChild,ElementRef } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ColorEvent } from 'ngx-color';
 import { ColorChromeModule } from "ngx-color/chrome";
-
+ 
 @Component({
   selector: 'app-generator',
   standalone: true,
@@ -10,9 +11,70 @@ import { ColorChromeModule } from "ngx-color/chrome";
   styleUrl: './generator.component.css'
 })
 export class GeneratorComponent {
-  @viewChild('memeCanvas', {static:false}) myCanvas:any;
-  preview(){
-    console.log("ok");
+  
+  @ViewChild('memeCanvas', { static: false }) memeCanvas: ElementRef | undefined;
+
+  topText:string = ""
+  bottomText:string = ""
+  fileEvent:any;
+  textColor:string = '#000000';
+  backgroundColor:string = "F9F9FB";
+
+  preview(e:any){
+    this.fileEvent = e;
+    let canvas = this.memeCanvas?.nativeElement;
+    let ctx = canvas.getContext('2d');
     
+    let render = new FileReader();
+    render.readAsDataURL(e.target.files[0]);
+    render.onload = function (event){
+      const img = new Image();
+      img.src = event.target?.result as string;
+      img.onload = function(){
+        ctx.drawImage(img,50,150,600,500);
+      }
+    }
+  }
+
+  drawText(){
+    
+    let canvas = this.memeCanvas?.nativeElement;
+    let ctx = canvas.getContext('2d');
+
+    ctx.clearRect(0,0, canvas.width, canvas.height)
+
+    ctx.fillStyle = this.backgroundColor;
+    ctx.fillRect(0,0, canvas.width, canvas.height)
+    
+    this.preview(this.fileEvent)
+
+    ctx.fillStyle = this.textColor;
+    ctx.font = '50px Comic Sans MS';
+    ctx.textAlign = 'center';
+    ctx.fillText(this.topText,canvas.width/2,100);
+    ctx.fillText(this.bottomText, canvas.width/2, 750)
+  }
+
+  canvasTextColor($event:ColorEvent){
+    
+    this.textColor = $event.color.hex;
+    
+    this.drawText();
+  }
+  canvasBgColor($event:ColorEvent){
+    this.backgroundColor = $event.color.hex;
+    this.drawText();
+  }
+
+  donwloadImg(){
+    let canvas = this.memeCanvas?.nativeElement;
+
+    let img = canvas.toDataURL('image/png');
+
+    let link = document.createElement('a');
+    link.download = 'memeImg.png';
+    link.href = img;
+    link.click()
+
   }
 }
